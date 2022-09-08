4f7588033c430b763888b8e98b6401ef606f6512
johangi <johangiaever@gmail.com>
2022-09-08 08:55:19 +0200

    adding assignment file

---
+++ bool.js
@@ -0,0 +1,29 @@
+// Oppgave 1
+// A blir true
+// B blir false
+// C blir true
+// D blir false
+
+// Oppgave 2
+// A blir true
+// B blir false
+// C) To likhetstegn betyr bare equals, da sammenligner den bare verdi, for eksempel "0==false" er sant, fordi 0 har samme verdi
+// som false. Med tre likhetstegn så er det strict equals, som betyr at begge elementene må være akkurat like for at bool verdien
+// skal være true. Det må ha lik type og lik verdi i den typen, for eksempel 1 er av typen "number", mens '1' er av typen "string"
+
+// Oppgave 3
+// A blir true
+// B blir false
+// C blir false
+// D blir true
+
+// Oppgave 4
+// A blir true
+// B blir true
+// C blir true
+
+// Oppgave 5
+// A blir false
+// B blir true
+// C blir true
+// D blir true
